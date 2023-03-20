fac77f96fc359abcd376a352ec1b6e2388ce9567
Leslie Yip <leslieyip02@gmail.com>
2023-03-20 23:43:34 +0800

    Adding card stacking option

--- src/game/card/deck.tsx
+++ src/game/card/deck.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Card from ".";
 import { CardHand, countCards } from "./hand";
 import { defaultIcons } from "./default";
@@ -5,22 +6,46 @@ import { defaultIcons } from "./default";
 interface DeckProps {
     cards: CardHand;
     drop?: boolean;
+    stack?: boolean;
 };
 
 const Deck = (props: DeckProps) => {
+    const [stack, setStack] = useState<boolean>(props.stack || false);
+
     return (
         <div className={`deck${props.drop ? " deck--drop" : ""}`}>
+            {
+                countCards(props.cards) > 0 && <button onClick={(e) => {
+                    e.stopPropagation();
+                    setStack((current) => !current);
+                }}>
+                    <i className="fa-solid fa-layer-group"></i>
+                </button>
+            }
             <div className="deck__cards">
                 {
                     countCards(props.cards) > 0
                         ? Object.entries(props.cards)
                             .filter(([card, quantity]) => quantity > 0)
                             .map(([card, quantity]) => {
-                                return <Card
-                                    key={card}
-                                    iconClassNames={defaultIcons[card]}
-                                    label={`${quantity} x ${card}`}
-                                />
+                                return stack
+                                    ? <Card
+                                        key={`stacked-${card}`}
+                                        iconClassNames={defaultIcons[card]}
+                                        label={`${quantity} x ${card}`}
+                                    />
+                                    : <>
+                                        {
+                                            Array(quantity).fill(0)
+                                                .map((_, i) => {
+                                                    return <Card
+                                                        key={`${card}-${i}`}
+                                                        iconClassNames={defaultIcons[card]}
+                                                        label={card}
+                                                    />
+                                                })
+                                        }
+                                    </>
                             })
                         : <Card
                             iconClassNames={["fa-solid", "fa-face-frown"]}
